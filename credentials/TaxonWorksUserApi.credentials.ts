215de6028e1cb0d1d36cc1fc58a55972db92238d
Geoff Ower <gdower@illinois.edu>
2025-07-01 23:32:37 -0500

    Update credentials

--- credentials/TaxonWorksUserApi.credentials.ts
+++ credentials/TaxonWorksUserApi.credentials.ts
@@ -1,7 +1,4 @@
-import {
-	ICredentialType,
-	INodeProperties,
-} from 'n8n-workflow';
+import { ICredentialType, INodeProperties } from 'n8n-workflow';
 
 export class TaxonWorksUserApi implements ICredentialType {
 	name = 'taxonWorksUserApi';
